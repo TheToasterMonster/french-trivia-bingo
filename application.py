28d4509d37eb55fd9dbcdbf8a30db39d12bdfdf1
TheToasterMonster <frankqiang5040@gmail.com>
2021-11-17 21:05:19 -0600

    added comments

--- application.py
+++ application.py
@@ -20,6 +20,7 @@ random.seed(datetime.now())
 def cleargame():
     """resets game"""
     global qs
+    # get a new questions list and clear past history
     qs = qbank.copy()
     past.clear()
     return
@@ -31,6 +32,7 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     else:
+        # reset game first before starting another
         cleargame()
         return redirect("/game")
 
@@ -39,9 +41,12 @@ def index():
 def game():
     """starts displaying bingo questions"""
     if request.method == "GET":
+        # send user back to main menu if out of questions
         if len(qs) == 0:
             return redirect("/")
+        # get a random question from the list
         past.append(qs.pop(random.randint(0, len(qs) - 1)))
+        # feed current question, last question, and past questions into the html file
         return render_template("game.html", question=past[len(past) - 1]["question"], runnerup=(past[len(past) - 2]["question"]
                if len(past) > 1 else ""), past=(past[:len(past) - 2][::-1] if len(past) > 2 else []))
     else:
@@ -54,10 +59,14 @@ def generate():
     if request.method == "GET":
         return render_template("generate.html")
     else:
+        # get number of boards from the user
         amount = int(request.form.get("amount"))
+        # initiate blank boards
         boards = [[] for i in range(amount)]
         for i in range(amount):
+            # get a fresh question bank for each board
             bank = qbank.copy()
+            # place an answer in each box in 5x5 grid, with middle being "FREE"
             for j in range(5):
                 boards[i].append([])
                 for k in range(5):
